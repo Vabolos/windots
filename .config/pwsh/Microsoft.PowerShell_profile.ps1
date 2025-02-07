3f0ded6bbc04ebbe45d7351cee4e050d33b5dbaa
Vabolos <79908586+Vabolos@users.noreply.github.com>
2025-02-07 09:42:46 +0100

    ✨New features and functions in pwsh profile

--- .config/pwsh/Microsoft.PowerShell_profile.ps1
+++ .config/pwsh/Microsoft.PowerShell_profile.ps1
@@ -7,13 +7,35 @@ Set-Alias ll ls
 Set-Alias g git
 Set-Alias vim nvim
 
+# Git Shortcuts
+function gs { git status }
+
+function ga { git add . }
+
+function gc { param($m) git commit -m "$m" }
+
+function gp { git push }
+
+# function g { __zoxide_z github }
+
+function gcl { git clone "$args" }
+
+function gcom {
+    git add .
+    git commit -m "$args"
+}
+function lazyg {
+    git add .
+    git commit -m "$args"
+    git push
+}
+
 # Auto-start
 ## choose between winfetch and fastfetch (uncomment one!)
 #cls;winfetch
 cls;fastfetch
 
 # Prompt
-# choose between clean-detailed and zen (uncomment one!)
 #oh-my-posh init pwsh --config 'C:\Users\lucfr\AppData\Local\Programs\oh-my-posh\themes\clean-detailed.omp.json' | Invoke-Expression
 oh-my-posh init pwsh --config 'C:\Users\lucfr\AppData\Local\Programs\oh-my-posh\themes\zen.toml' | Invoke-Expression
 
@@ -24,6 +46,29 @@ function whereis ($command) {
 }
 
 # Modules
+# Enhanced PSReadLine Configuration
+$PSReadLineOptions = @{
+    EditMode = 'Windows'
+    HistoryNoDuplicates = $true
+    HistorySearchCursorMovesToEnd = $true
+    Colors = @{
+        Command = '#87CEEB'  # SkyBlue (pastel)
+        Parameter = '#98FB98'  # PaleGreen (pastel)
+        Operator = '#FFB6C1'  # LightPink (pastel)
+        Variable = '#DDA0DD'  # Plum (pastel)
+        String = '#FFDAB9'  # PeachPuff (pastel)
+        Number = '#B0E0E6'  # PowderBlue (pastel)
+        Type = '#F0E68C'  # Khaki (pastel)
+        Comment = '#D3D3D3'  # LightGray (pastel)
+        Keyword = '#8367c7'  # Violet (pastel)
+        Error = '#FF6347'  # Tomato (keeping it close to red for visibility)
+    }
+    PredictionSource = 'History'
+    PredictionViewStyle = 'ListView'
+    BellStyle = 'None'
+}
+Set-PSReadLineOption @PSReadLineOptions
+
 Import-Module Terminal-Icons
 Set-PSReadLineKeyHandler -Key Tab -Function Complete
 Set-PSReadLineOption -PredictionViewStyle ListView
